2b73dd1181a746b420558cfbd3c47094d59feb09
Lavina Pinto <pinto.l@northeastern.edu>
2021-12-03 20:05:04 -0500

    Push code for Recipient and Funcding classes

--- src/Business/Entity/Recipient.java
+++ src/Business/Entity/Recipient.java
@@ -9,6 +9,34 @@ package Business.Entity;
  *
  * @author Lavina
  */
-public class Recipient {
+public class Recipient extends Person implements Comparable<Recipient>{
+    
+    private int priorityNo;
+    private String organType;
+  
+    public void setPriorityNo(int priorityNo) {
+        this.priorityNo = priorityNo;
+    }
+    
+    public int getPriorityNo() {
+        return priorityNo;
+    }
+
+    public void setOrganType(String organType) {
+        this.organType = organType;
+    }
+    
+        public String getOrganType() {
+        return organType;
+    }
+        
+    @Override
+    public String toString() {
+        return this.getPersonName();
+    }
+    
+    public int compareTo(Recipient o) {
+		return Integer.compare(o.getPriorityNo(), this.getPriorityNo());
+	}
     
 }
