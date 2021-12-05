d115ab85c648955e562c2990741f76a39276309a
Lavina Pinto <pinto.l@northeastern.edu>
2021-12-04 23:39:10 -0500

    hospital enterprise roles

--- src/Business/Role/Role.java
+++ src/Business/Role/Role.java
@@ -6,7 +6,6 @@ package Business.Role;
 
 import Business.EcoSystem;
 
-import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
 
@@ -15,15 +14,21 @@ import javax.swing.JPanel;
  * @author amey
  */
 public abstract class Role {
-    
-    public enum RoleType{
-        RestaurantAdmin("RestaurantAdmin"),
-        Customer("Customer"),
-        DeliveryMan("Delivery"),
-        SysAdmin("Sysadmin");
-        
+
+    public enum RoleType {
+        ApplicantRole("Applicant"),
+        CampaignManagerRole("Campaign Manager"),
+        CorporateManagerRole("Corporate Manager"),
+        DoctorRole("Doctor"),
+        EnterpriseManagerRole("Enterprise Manager"),
+        InternalLabAssistantRole("Internal Lab Assistant"),
+        SystemAdminRole("System Admin"),
+        LabAssistantRole("Lab Assistant"),
+        TrustManagerRole("Trust Manager");
+
         private String value;
-        private RoleType(String value){
+
+        private RoleType(String value) {
             this.value = value;
         }
 
@@ -36,15 +41,38 @@ public abstract class Role {
             return value;
         }
     }
-    
-    public abstract JPanel createWorkArea(JPanel userProcessContainer, 
-            UserAccount account,  
+
+    public Role createRole(Role.RoleType roleType) {
+        Role roleObj = null;
+        if (roleType.equals(RoleType.ApplicantRole)) {
+            roleObj = new ApplicantRole();
+        } else if (roleType.equals(RoleType.CampaignManagerRole)) {
+            roleObj = new CampaignManagerRole();
+        } else if (roleType.equals(RoleType.DoctorRole)) {
+            roleObj = new DoctorRole();
+        } else if (roleType.equals(RoleType.LabAssistantRole)) {
+            roleObj = new LabAssistantRole();
+        } else if (roleType.equals(RoleType.InternalLabAssistantRole)) {
+            roleObj = new InternalLabAssistantRole();
+        } else if (roleType.equals(RoleType.CorporateManagerRole)) {
+            roleObj = new CorporateManagerRole();
+        } else if (roleType.equals(RoleType.TrustManagerRole)) {
+             roleObj = new TrustManagerRole();
+        } else if (roleType.equals(RoleType.SystemAdminRole)){
+             roleObj = new TrustManagerRole();
+        } else if (roleType.equals(RoleType.EnterpriseManagerRole)){
+             roleObj = new EnterpriseManagerRole();
+        }
+       return roleObj;
+    }
+
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,
+            UserAccount account,
             EcoSystem business);
 
     @Override
     public String toString() {
         return this.getClass().getName();
     }
-    
-    
+
 }
