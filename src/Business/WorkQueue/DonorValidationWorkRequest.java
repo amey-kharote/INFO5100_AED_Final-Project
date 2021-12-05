b2f41180d5cb6fedd28fa178aa9115a04ac19187
Lavina Pinto <pinto.l@northeastern.edu>
2021-12-05 15:53:15 -0500

    Push code for WorkRequest classes

--- src/Business/WorkQueue/DonorValidationWorkRequest.java
+++ src/Business/WorkQueue/DonorValidationWorkRequest.java
@@ -9,6 +9,72 @@ package Business.WorkQueue;
  *
  * @author amey
  */
-public class DonorValidationWorkRequest {
+public class DonorValidationWorkRequest extends WorkRequest{
+    
+    private String donorName;
+    private int donorAge;
+    private boolean physicalInjury;
+    private boolean drinkingHistory;
+    private boolean smokingHistory;
+    private boolean diabetic;
+    private boolean heartDisease;
+
+    public String getDonorName() {
+        return donorName;
+    }
+
+    public void setDonorName(String donorName) {
+        this.donorName = donorName;
+    }
+
+    public int getDonorAge() {
+        return donorAge;
+    }
+
+    public void setDonorAge(int donorAge) {
+        this.donorAge = donorAge;
+    }
+
+    public boolean isPhysicalInjury() {
+        return physicalInjury;
+    }
+
+    public void setPhysicalInjury(boolean physicalInjury) {
+        this.physicalInjury = physicalInjury;
+    }
+
+    public boolean isDrinkingHistory() {
+        return drinkingHistory;
+    }
+
+    public void setDrinkingHistory(boolean drinkingHistory) {
+        this.drinkingHistory = drinkingHistory;
+    }
+
+    public boolean isSmokingHistory() {
+        return smokingHistory;
+    }
+
+    public void setSmokingHistory(boolean smokingHistory) {
+        this.smokingHistory = smokingHistory;
+    }
+
+    public boolean isDiabetic() {
+        return diabetic;
+    }
+
+    public void setDiabetic(boolean diabetic) {
+        this.diabetic = diabetic;
+    }
+
+    public boolean isHeartDisease() {
+        return heartDisease;
+    }
+
+    public void setHeartDisease(boolean heartDisease) {
+        this.heartDisease = heartDisease;
+    }
+    
+    
     
 }
--- src/Business/WorkQueue/LabTestWorkRequest.java
+++ src/Business/WorkQueue/LabTestWorkRequest.java
@@ -9,6 +9,35 @@ package Business.WorkQueue;
  *
  * @author amey
  */
-public class LabTestWorkRequest {
+public class LabTestWorkRequest extends WorkRequest{
+    
+    private String patientName;
+    private String attachedFile;
+    private String labResult;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getAttachedFile() {
+        return attachedFile;
+    }
+
+    public String getLabResult() {
+        return labResult;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public void setAttachedFile(String attachedFile) {
+        this.attachedFile = attachedFile;
+    }
+
+    public void setLabResult(String labResult) {
+        this.labResult = labResult;
+    }
+    
     
 }
