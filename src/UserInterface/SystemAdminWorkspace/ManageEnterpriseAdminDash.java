cacf55d40daaa284c986fc120f2f3c95a05d4040
Ninoshka Pinto <pinto.n@northeastern.edu>
2021-12-05 16:34:42 -0500

    adding system admin screens

--- src/UserInterface/SystemAdminWorkspace/ManageEnterpriseAdminDash.java
+++ src/UserInterface/SystemAdminWorkspace/ManageEnterpriseAdminDash.java
@@ -5,19 +5,65 @@
  */
 package UserInterface.SystemAdminWorkspace;
 
+import Business.Account.Account;
+import Business.EcoSystem;
+import Business.Employee.Employee;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Role.EnterpriseManagerRole;
+import Business.UserAccount.UserAccount;
+import Business.Utils.Utils;
+import java.awt.CardLayout;
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Amey
  */
 public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
 
+    EcoSystem ecoSystem;
+    JPanel rightJPanel;
     /**
      * Creates new form ManageEnterpriseAdminDash
      */
-    public ManageEnterpriseAdminDash() {
+    public ManageEnterpriseAdminDash(JPanel rightJPanel, EcoSystem ecoSystemObj) {
         initComponents();
+        this.ecoSystem = ecoSystemObj;
+        this.rightJPanel = rightJPanel;
+        populateEnterpriseTable();
+        populateNetworkList();
     }
+   
+    private void populateNetworkList() {
+        cityNetworkDropdown.removeAllItems();
+        for (Network network : ecoSystem.getNetworks()) cityNetworkDropdown.addItem(network);
+        
+    }
+    
+    private void populateEnterpriseTable() {
+        DefaultTableModel model = (DefaultTableModel) enterpriseAdminTable.getModel();
+        model.setRowCount(0);
+        for (Network network : ecoSystem.getNetworks()) {
+            for (Enterprise enterpriseObj : network.getEnterpriseDirectory().getEnterpriseList()) {
+                for (UserAccount userAccount : enterpriseObj.getUserAccountDirectory().getUserAccountList()) {
+                    Object[] rowObj = new Object[3];
+                    rowObj[0] = enterpriseObj.getName();
+                    rowObj[1] = network.getName();
+                    rowObj[2] = userAccount.getUsername();
+                    model.addRow(rowObj);
+                }
+            }
+        }
+    }     
 
+    private void populateEnterpriseList(Network networkObj) {
+        enterpriseNetworkDropdown.removeAllItems();
+        for (Enterprise enterprise : networkObj.getEnterpriseDirectory().getEnterpriseList()) enterpriseNetworkDropdown.addItem(enterprise);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,7 +75,7 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
 
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        enterpriseJTable = new javax.swing.JTable();
+        enterpriseAdminTable = new javax.swing.JTable();
         formCityLabel = new javax.swing.JLabel();
         cityNetworkDropdown = new javax.swing.JComboBox();
         formenterpriseLabel = new javax.swing.JLabel();
@@ -46,9 +92,9 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
         jPanel1.setBackground(new java.awt.Color(204, 204, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        enterpriseJTable.setBackground(new java.awt.Color(0, 153, 255));
-        enterpriseJTable.setFont(new java.awt.Font("Times New Roman", 1, 23)); // NOI18N
-        enterpriseJTable.setModel(new javax.swing.table.DefaultTableModel(
+        enterpriseAdminTable.setBackground(new java.awt.Color(0, 153, 255));
+        enterpriseAdminTable.setFont(new java.awt.Font("Times New Roman", 1, 23)); // NOI18N
+        enterpriseAdminTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null},
                 {null, null, null},
@@ -67,7 +113,7 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(enterpriseJTable);
+        jScrollPane1.setViewportView(enterpriseAdminTable);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 920, 140));
 
@@ -164,10 +210,43 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
 
     private void SubmitBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SubmitBtnActionPerformed
         // TODO add your handling code here:
+        Utils util = new Utils();
+        Enterprise enterpriseObj = (Enterprise) enterpriseNetworkDropdown.getSelectedItem();
+        String emailId = formEmailTextField.getText();
+        String password = String.valueOf(formPasswordField.getPassword());
+
+        if (!(util.notNullOrEmpty(password) || util.notNullOrEmpty(emailId))) {
+            JOptionPane.showMessageDialog(null, "Please enter emailId and password");
+            return;
+        }
+        if (!util.isPasswordValid(password)) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid email id.");
+            return;
+        }
+        if (!util.isEmaildIdvalid(emailId)) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid email id.");
+            return;
+        }      
+
+        Employee employee = enterpriseObj.getEmployeeDirectory().createEmployee(emailId);
+        UserAccount account = enterpriseObj.getUserAccountDirectory().createUserAccount(emailId, password, employee, new EnterpriseManagerRole());
+        
+        formEmailTextField.setText("");
+        formPasswordField.setText("");
+        
+        populateEnterpriseTable();
     }//GEN-LAST:event_SubmitBtnActionPerformed
 
     private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBtnActionPerformed
         // TODO add your handling code here:
+        rightJPanel.remove(this);
+        Component[] componentArray = rightJPanel.getComponents();
+        Component componentObj = componentArray[componentArray.length - 1];
+        SystemAdminDashboard sysAdminScreen = (SystemAdminDashboard) componentObj;
+        CardLayout prevLayout = (CardLayout) rightJPanel.getLayout();
+        // populate tree on admin screen
+        sysAdminScreen.populateTree();
+        prevLayout.previous(rightJPanel);
     }//GEN-LAST:event_backBtnActionPerformed
 
     private void enterpriseNetworkDropdownActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_enterpriseNetworkDropdownActionPerformed
@@ -175,7 +254,8 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
     }//GEN-LAST:event_enterpriseNetworkDropdownActionPerformed
 
     private void cityNetworkDropdownActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cityNetworkDropdownActionPerformed
-
+        Network networkObj = (Network) cityNetworkDropdown.getSelectedItem();
+       if (networkObj != null)  populateEnterpriseList(networkObj);
     }//GEN-LAST:event_cityNetworkDropdownActionPerformed
 
 
@@ -183,7 +263,7 @@ public class ManageEnterpriseAdminDash extends javax.swing.JPanel {
     private javax.swing.JButton SubmitBtn;
     private javax.swing.JButton backBtn;
     private javax.swing.JComboBox cityNetworkDropdown;
-    private javax.swing.JTable enterpriseJTable;
+    private javax.swing.JTable enterpriseAdminTable;
     private javax.swing.JComboBox enterpriseNetworkDropdown;
     private javax.swing.JLabel formCityLabel;
     private javax.swing.JTextField formEmailTextField;
